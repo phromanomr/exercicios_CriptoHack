4f9d4fdb75c488496eda7c31ca339df467db88f8
Phelipe <phromanomr@gmail.com>
2026-04-03 11:18:44 -0300

    feat: resolucao do enc3 da Intro

---
+++ Introducao/intro_enc3.py
@@ -0,0 +1,11 @@
+import base64
+
+texto_hex = "72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf"
+
+texto_plano = bytes.fromhex(texto_hex)
+
+print(texto_plano)
+
+texto_base64 = base64.b64encode(texto_plano)
+
+print(texto_base64)
