083dd6aaa9379408171ef0d1ba30a237b6787a84
Phelipe <phromanomr@gmail.com>
2026-04-03 11:29:15 -0300

    feat: resolucao do enc4 da Intro

---
+++ Introducao/intro_enc4.py
@@ -0,0 +1,7 @@
+from Crypto.Util.number import *
+
+texto_crifrado = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
+
+texto_plano = long_to_bytes(texto_crifrado)
+
+print(texto_plano)
